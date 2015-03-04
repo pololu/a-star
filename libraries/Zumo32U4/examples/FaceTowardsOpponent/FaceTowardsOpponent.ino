805c3bdd3bd5244da9339e9d3a7be19c46404e16
David (Pololu) <dev-david@pololu.com>
2015-03-03 19:55:58 -0800

    Zumo32U4: Addeded lots of comments to FaceTowardsOpponents.ino.

--- libraries/Zumo32U4/examples/FaceTowardsOpponent/FaceTowardsOpponent.ino
+++ libraries/Zumo32U4/examples/FaceTowardsOpponent/FaceTowardsOpponent.ino
@@ -12,23 +12,47 @@ Zumo32U4Motors motors;
 Zumo32U4ProximitySensors proxSensors;
 Zumo32U4ButtonA buttonA;
 
+// A sensors reading must be greater than or equal to this
+// threshold in order for the program to consider that sensor as
+// seeing an object.
+const uint8_t sensorThreshold = 1;
+
+// The maximum speed to drive the motors while turning.  400 is
+// full speed.
+const uint16_t turnSpeedMax = 400;
+
+// The minimum speed to drive the motors while turning.  100 if
+// full speed.
+const uint16_t turnSpeedMin = 100;
+
+// The amount to decrease the motor speed by during each cycle
+// when an object is seen.
+const uint16_t deceleration = 10;
+
+// The amount to increase the speed by during each cycle when an
+// object is not seen.
+const uint16_t acceleration = 10;
+
 #define LEFT 0
 #define RIGHT 1
 
-const uint8_t sensorThreshold = 1;
-const uint16_t turnSpeedMax = 400; // was 300
-const uint16_t turnSpeedMin = 100;
-const uint16_t deceleration = 10;
-const uint16_t acceleration = 10;
-
+// Stores the last indication from the sensors about what
+// direction to turn to face the object.  When no object is seen,
+// this variable helps us make a good guess about which direction
+// to turn.
 bool senseDir = RIGHT;
+
+// True if the robot is turning left (counter-clockwise).
 bool turningLeft = false;
+
+// True if the robot is turning right (clockwise).
 bool turningRight = false;
-uint16_t lastTimeObjectSeen = 0;
+
+// If the robot is turning, this is the speed it will use.
 uint16_t turnSpeed = turnSpeedMax;
 
-uint8_t cyclesSinceLastReversal = 255;
-uint8_t shortReversalCount = 0;
+// The time, in milliseconds, when an object was last seen.
+uint16_t lastTimeObjectSeen = 0;
 
 void setup()
 {
@@ -64,25 +88,37 @@ void stop()
 
 void loop()
 {
+  // Read the front proximity sensor and gets its left value (the
+  // amount of reflectance detected while using the left LEDs)
+  // and right value.
   proxSensors.read();
   uint8_t leftValue = proxSensors.countsFrontWithLeftLeds();
   uint8_t rightValue = proxSensors.countsFrontWithRightLeds();
 
+  // Determine if an object is visible or not.
   bool objectSeen = leftValue >= sensorThreshold || rightValue >= sensorThreshold;
 
   if (objectSeen)
   {
+    // An object is visible, so we will start decelerating in
+    // order to help the robot find the object without
+    // overshooting or oscillating.
     turnSpeed -= deceleration;
   }
   else
   {
+    // An object is not visible, so we will accelerate in order
+    // to help find the object sooner.
     turnSpeed += acceleration;
   }
+
+  // Constrain the turn speed so it is between turnSpeedMin and
+  // turnSpeedMax.
   turnSpeed = constrain(turnSpeed, turnSpeedMin, turnSpeedMax);
 
   if (objectSeen)
   {
-    // Object seen.
+    // An object seen.
     ledYellow(1);
 
     lastTimeObjectSeen = millis();
@@ -91,22 +127,29 @@ void loop()
 
     if (leftValue < rightValue)
     {
+      // The right value is greater, so the object is probably
+      // closer to the robot's right LEDs, which means the robot
+      // is not facing it directly.  Turn to the right to try to
+      // make it more even.
       turnRight();
       senseDir = RIGHT;
     }
     else if (leftValue > rightValue)
     {
+      // The left value is greater, so turn to the left.
       turnLeft();
       senseDir = LEFT;
     }
     else
     {
+      // The values are equal, so stop the motors.
       stop();
     }
   }
   else
   {
-    // No object seen, so just keep turning.
+    // No object is seen, so just keep turning in the direction
+    // that we last sensed the object.
     ledYellow(0);
 
     if (senseDir == RIGHT)
