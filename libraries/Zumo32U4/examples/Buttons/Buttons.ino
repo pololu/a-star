4c396f4f7d36efe03a7bae2cfa49ff3fed267181
RyanTM (Pololu) <dev-ryantm@pololu.com>
2015-03-05 14:49:07 -0800

    add lcd and fix bugs in Zumo32U4 Buttons.ino

--- libraries/Zumo32U4/examples/Buttons/Buttons.ino
+++ libraries/Zumo32U4/examples/Buttons/Buttons.ino
@@ -9,13 +9,19 @@ interface with a user pushbutton on the Zumo 32U4. */
 Zumo32U4ButtonA buttonA;
 Zumo32U4ButtonB buttonB;
 Zumo32U4ButtonC buttonC;
+Zumo32U4LCD lcd;
 
 void setup()
 {
+  lcd.clear();
+  lcd.print(F("Press A"));
+
   /* Method 1: Use the waitForButton() function, which blocks and
    * doesn't return until a button press and release are
    * detected. This function takes care of button debouncing. */
   buttonA.waitForButton();
+
+  lcd.clear();
 }
 
 void loop()
@@ -28,6 +34,13 @@ void loop()
   {
     // Whenever the button is pressed, turn on the yellow LED.
     ledYellow(1);
+
+    Serial.println(F("Button B was pressed."));
+
+    lcd.clear();
+    lcd.print(F("Button B"));
+    lcd.gotoXY(0,1);
+    lcd.print(F("pressed "));
   }
   else
   {
@@ -42,13 +55,18 @@ void loop()
    * care of button debouncing. */
   if (buttonC.getSingleDebouncedPress())
   {
-    Serial.println(F("Button B was pressed."));
+    Serial.println(F("Button C was pressed."));
+
+    lcd.clear();
+    lcd.print(F("Button C"));
+    lcd.gotoXY(0,1);
+    lcd.print(F("pressed"));
   }
 
   /* If you use non-blocking functions like isPressed() and
    * getSingleDebouncedPress(), then you can monitor multiple
    * buttons at the same time and also take care of other tasks
-   * at the same time.  In this example, we blink the green LED
+   * at the same time.  In this example, we blink the red LED
    * while monitoring the buttons. */
   ledRed(millis() % 1024 < 100);
 }
